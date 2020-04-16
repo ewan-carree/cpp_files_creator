d242089e8404e296c85dde3ee24288bda0828f55
ewan-carree <62911876+ewan-carree@users.noreply.github.com>
2020-04-16 11:45:43 +0200

    update main.py

--- main.py
+++ main.py
@@ -3,9 +3,25 @@
 import lib
 import files
 
+global userName
+global time
+userName = lib.get_userName()
+time = lib.get_time()
+
 def main():
-	#extraxt_args
-	#create_file(arg[0])
+	main, header, namespace = lib.extract_args()
+	lib.create_file(header)
+	lib.set_path(lib.get_path(), main)
+	lib.add_GNU()
+
+	mainFile= files.Main_cpp(main)
+	mainFile.create_main_cpp(userName, time, header)
+	
+	headercppFile = files.Header_cpp(header, namespace)
+	headercppFile.create_header_cpp(header)
+
+	headerhppFile = files.Header_hpp(header, namespace)
+	headerhppFile.create_header_hpp()
 
 
 
